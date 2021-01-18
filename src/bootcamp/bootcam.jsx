35aa6f77da90b3ade31a1c583b03bed704ebecc6
Biljana <zabiljanupavlovic@gmail.com>
2021-01-18 22:17:11 +0100

    positions fixed

--- src/bootcamp/bootcam.jsx
+++ src/bootcamp/bootcam.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import './bootcamp.css';
+/** @format */
+
+import React from "react";
+import "./bootcamp.css";
 
 export default function Bootcam() {
-    return (
-        <div className = 'bootcamp'>
-            <h2>ITBootcamp</h2>
-            <div className="bootcamp-div">
+  return (
+    <div className="bootcamp">
+      <h2>ITBootcamp</h2>
 
-        <img src = 'images/itbootcamp/itbootcamp.png' id = "itbootcamp-logo" alt = 'itbootcamp logo' />   
-     <dic className="ico">
+      <img
+        src="images/itbootcamp/itbootcamp.png"
+        id="itbootcamp-logo"
+        alt="itbootcamp logo"
+      />
 
+      <img src="images/itbootcamp/git.png" id="git-ico" alt="git logo" />
 
-        <img src = 'images/itbootcamp/git.png' id = 'git-ico' alt = 'git logo' />   
-       
-        <img src = 'images/itbootcamp/js.png' id = 'js-ico' alt = 'javaScript logo' />   
-        <img src = 'images/itbootcamp/react.png' id = 'react-ico' alt = 'react logo' />   
-     </dic>
+      <img src="images/itbootcamp/js.png" id="js-ico" alt="javaScript logo" />
+      <img src="images/itbootcamp/react.png" id="react-ico" alt="react logo" />
 
-<img src = 'images/itbootcamp/zaStolom.png' id = 'school-woman' alt = 'women at school' />
-            </div>
-        </div>
-    )
+      <img
+        src="images/itbootcamp/zaStolom.png"
+        id="school-woman"
+        alt="women at school"
+      />
+    </div>
+  );
 }
--- src/stillStudy/stillStudy.jsx
+++ src/stillStudy/stillStudy.jsx
@@ -1,21 +1,60 @@
-import React from 'react'
-import './stillStudy.css'
+/** @format */
+
+import React from "react";
+import "./stillStudy.css";
 
 export default function StillStudy() {
-    return (
-        <div className = 'study' >
-            <h2>Studying scene</h2>
-            <img src="images/stydy/deved.jpg" alt="study ico" className = "study-ico" />
-            <img src="images/stydy/kampster.jpg" alt="study ico" className = "study-ico" />
-            <img src="images/stydy/netninja.jpg" alt="study ico" className = "study-ico" />
-            
-            <img src="images/stydy/study.png" alt="life long learning"/>
-            
-            <img src="images/stydy/traversy.jpg" alt="study ico" className = "study-ico" />
-            <img src="images/stydy/webcon.png" alt="study ico" className = "study-ico" />
-            <img src="images/stydy/webdev.jpg" alt="study ico" className = "study-ico" />
-            <img src="images/stydy/mintbean.png" alt="study ico" className = "study-ico" />
+  return (
+    <div className="study">
+      <h2 className="study-headline">Studying scene</h2>
+      <img
+        src="images/stydy/deved.jpg"
+        alt="study ico"
+        className="study-ico"
+        id="deved"
+      />
+      <img
+        src="images/stydy/kampster.jpg"
+        alt="study ico"
+        className="study-ico"
+        id="kampster"
+      />
+      <img
+        src="images/stydy/netninja.jpg"
+        alt="study ico"
+        className="study-ico"
+        id="ninja"
+      />
+      <img
+        src="images/stydy/traversy.jpg"
+        alt="study ico"
+        className="study-ico"
+        id="traversy"
+      />
+      <img
+        src="images/stydy/study.png"
+        alt="life long learning"
+        id="avatar-study"
+      />
 
-        </div>
-    )
+      <img
+        src="images/stydy/webcon.png"
+        alt="study ico"
+        className="study-ico"
+        id="webcon"
+      />
+      <img
+        src="images/stydy/webdev.jpg"
+        alt="study ico"
+        className="study-ico"
+        id="webdev"
+      />
+      <img
+        src="images/stydy/mintbean.png"
+        alt="study ico"
+        className="study-ico"
+        id="mintbean"
+      />
+    </div>
+  );
 }
