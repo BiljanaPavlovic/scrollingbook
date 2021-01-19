528bc298d9e0702df244c16516f8b828f72762fa
Biljana <zabiljanupavlovic@gmail.com>
2021-01-19 01:48:34 +0100

    some animations added

--- src/party/party.jsx
+++ src/party/party.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import './party.css';
+/** @format */
 
+import React from "react";
+import "./party.css";
 
 export default function Party() {
-    return (
-        <div className = 'party'>
-            <h2>Party</h2>
-            <img src="images/party/partyGirl.png" alt="party girl"/>
-        </div>
-    )
+  return (
+    <div className="party">
+      <h2>Party</h2>
+      <img src="images/party/partyGirl.png" id="party-girl" alt="party girl" />
+    </div>
+  );
 }
